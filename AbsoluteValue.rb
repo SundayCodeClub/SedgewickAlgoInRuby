95ac4588e56d65b651d8e6e81041c8a149fae37b
nicole pank <nicole.pank@gmail.com>
2014-09-14 20:32:57 -0500

    remove superfluous method

--- AbsoluteValue.rb
+++ AbsoluteValue.rb
@@ -6,13 +6,9 @@ def absolute(x)
   end
 end
 
-def double_absolute(x)
-  if x < 0.0
-    return -x
-  else
-    return x
-  end
-end
-
-# These seem to be doing the same thing;
-# Both work with integers and floats. Why are they different?
+# double method is unnecessary in Ruby, but not Java. In Java,
+# public static double abs(double x)
+# {
+#    if (x < 0.0) return -x;
+#    else         return  x;
+# }
